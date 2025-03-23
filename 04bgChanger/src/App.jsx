e4c11f5c62dabf1aa6375e1b54fb84324aeab8a6
Sanidhya Ojha <ojhaayush21102002@gmail.com>
2025-03-23 15:51:37 +0530

    kkk

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-            onClick={() => setColor("Red")}  {/* Onclick always takes a fn not a value of a fn, that's why we are using setColor within fatarrow not directly setColor("Red"). */}
+            onClick={() => setColor("Red")}  /* Onclick always takes a fn not a value of a fn, that's why we are using setColor within fatarrow not directly setColor("Red"). */
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{backgroundColor: "red"}}
             >Red
